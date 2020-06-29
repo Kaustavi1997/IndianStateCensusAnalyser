c19ebf3be3942e69833633b29ff6ea93bb870dcf
“Kaustavi <kaustavisen8@gmail.com>
2020-06-29 19:35:37 +0530

    Refactored:Handeled Exception

--- src/main/java/com/bridgelabz/service/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/CensusAnalyser.java
@@ -31,6 +31,9 @@ public class CensusAnalyser {
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }catch (IllegalStateException e){
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
         }
     }
 }
