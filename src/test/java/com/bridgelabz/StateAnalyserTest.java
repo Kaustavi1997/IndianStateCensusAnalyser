ed956b391811729719f94ace3e76c4fb212ea257
“Kaustavi <kaustavisen8@gmail.com>
2020-06-30 00:02:05 +0530

    Added:TC2.4-delimiter incorrect

--- src/test/java/com/bridgelabz/StateAnalyserTest.java
+++ src/test/java/com/bridgelabz/StateAnalyserTest.java
@@ -9,6 +9,7 @@ public class StateAnalyserTest {
     private static final String INDIA_STATE_CODE_CSV_PATH = "./src/test/resources/IndiaStateCode.csv";
     private static final String WRONG_STATE_CSV_PATH = "./src/main/resources/IndiaStateCode.csv";
     private static final String NOT_STATE_CSV_FILE = "./src/test/resources/IndiaStateCode.txt";
+    private static final String DELIMITER_STATE_INCORRECT = "./src/test/resources/DelimiterStateIncorrect.csv";
 
     @Test
     public void givenIndianStateCSVFileReturnsCorrectRecords() {
@@ -39,6 +40,17 @@ public class StateAnalyserTest {
             stateAnalyser.loadIndiaStateData(NOT_STATE_CSV_FILE);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+    @Test
+    public void givenIndiaStateData_WithWrongFileDelimiter_ShouldThrowException() {
+        try {
+            StateAnalyser stateAnalyser = new StateAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            stateAnalyser.loadIndiaStateData(DELIMITER_STATE_INCORRECT);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.DELIMITER_HEADER_ISSUE,e.type);
         }
     }
 
