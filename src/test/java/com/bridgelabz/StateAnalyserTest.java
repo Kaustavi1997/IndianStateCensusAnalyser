c4452b6f609771338cb84abe63112b5a2d3e7e35
“Kaustavi <kaustavisen8@gmail.com>
2020-06-29 23:53:37 +0530

    Added:TC2.2-Given Indian state data with wrong file should throw exception

--- src/test/java/com/bridgelabz/StateAnalyserTest.java
+++ src/test/java/com/bridgelabz/StateAnalyserTest.java
@@ -7,6 +7,7 @@ import org.junit.rules.ExpectedException;
 
 public class StateAnalyserTest {
     private static final String INDIA_STATE_CODE_CSV_PATH = "./src/test/resources/IndiaStateCode.csv";
+    private static final String WRONG_STATE_CSV_PATH = "./src/main/resources/IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -15,6 +16,17 @@ public class StateAnalyserTest {
             int numOfRecords = stateAnalyser.loadIndiaStateData(INDIA_STATE_CODE_CSV_PATH);
             Assert.assertEquals(29, numOfRecords);
         } catch (CensusAnalyserException e) {
+        }
+    }
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            StateAnalyser stateAnalyser = new StateAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            stateAnalyser.loadIndiaStateData(WRONG_STATE_CSV_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 
